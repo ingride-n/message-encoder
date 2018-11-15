fbc81fc49f3e684c3ca9ccfab5bd9c815fb171b4
Ingride <33769099+ingride-n@users.noreply.github.com>
2018-11-15 00:09:05 -0500

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -9,15 +9,23 @@ web_hook_url = '[enter url here]'
 
 today = date.today()
 
+# Get number of days until winter break
+
 holiday = date(today.year, 12, 18)
 time_to_holiday = abs(holiday - today)
 delta_t = time_to_holiday.days
 
+# Input message
+
 input_msg = "We are "+str(delta_t)+" days away from winter break"
+
+# Send input message to blackbox to encode
 
 K = blackbox.positions()
 key = blackbox.key_gen(K, input_msg)
 msg_x = blackbox.scramble(K, input_msg)
+
+# Save blackbox output to a file
 
 with open('bot-messages.txt','a') as file:
 	file.write(msg_x+"\t")
@@ -25,6 +33,7 @@ with open('bot-messages.txt','a') as file:
 	file.write(str(datetime.now())+"\t")
 	file.close() 
 
-final_msg = {'text':msg_x}
+# Post message to Slack
 
+final_msg = {'text':msg_x}
 requests.post(web_hook_url,data=json.dumps(final_msg))
